0d9512a9c3d5c8c672afb7d3e3be340d08035d48
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-10 16:45:23 +0100

    relational database query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,23 @@ const {
   GraphQLID,
 } = require('graphql');
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -24,6 +41,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent) {
