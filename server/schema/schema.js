b7a4da2d762c94ac2011c3680601cbcecde41d5c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-10 16:16:48 +0100

    graphql scheme & root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,37 @@
-const { buildSchema } = require('graphql');
+const { projects, clients } = require('../sampleData');
+
+const {
+  buildSchema,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+} = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -6,3 +39,7 @@ const schema = buildSchema(`
     hello: String
   }
 `);
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
